679b76165cb548eb3dad68969cadabfb0926790d
Ekalape <ekalape@outlook.it>
2023-06-19 07:56:41 +0200

    feat: implement helper

--- src/index.js
+++ src/index.js
@@ -12,8 +12,9 @@ if (fullArgs.length === 0 || !fullArgs.find((x) => x.match(/--username=/i)))
   console.log('Please enter your name in format -- --username=your_name');
 else {
   userName = fullArgs[0].split('=')[1] || 'guest';
-  console.log(`Welcome to the File Manager, ${userName}!`);
+  console.log(`Welcome to the File Manager, ${userName}!\n`);
   console.log(`You are currently in ${homePath}`);
+  console.log(`Please, type your commands, for full list type 'help'`);
 
   process.stdin.on('data', (data) => listenCommands(userName, data));
   process.on('SIGINT', () => {
--- src/operational/commands.js
+++ src/operational/commands.js
@@ -7,6 +7,7 @@ const homePath = path.join(__dirname, 'files');
 let userName = 'guest';
 
 const fileOperation = {
+  help: commandsList,
   exit: exitFileManager,
   //up: goUpper,
   //cd: goToTheDir,
@@ -27,19 +28,38 @@ async function listenCommands(name, data) {
   userName = name;
   const args = data.toString().split(' ');
   const command = args[0].trim();
-  if (command !== 'exit') console.log(`You are currently in ${homePath}`);
+  if (command !== 'exit') console.log(`You are currently in ${homePath}\n`);
 
-  if (command) {
+  if (command && Object.keys(fileOperation).includes(command)) {
     await fileOperation[command](args.slice(1));
   } else {
-    console.log('undefined command');
-    process.exit();
+    console.log('Unknown command\n');
+    //process.exit();
   }
 }
 
 export default listenCommands;
 
+function commandsList() {
+  const helpList = [
+    ['up', 'go upper from current directory', 'up'],
+    ['cd', 'go to dedicated folder from current directory', 'cd path_to_directory'],
+    ['ls', 'print list of all files and folders in current directory', 'ls'],
+    ['add', 'add an empty file to the working directory', 'add path_to_folder'],
+    ['cat', 'read a file', 'cat path_to_file'],
+    ['rn', 'rename a file ', 'rn path_to_file new_filename'],
+    ['cp', 'copy a file ', 'cp path_to_file path_to_new_directory'],
+    ['mv', 'move a file ', 'mv path_to_file path_to_new_directory'],
+    ['rm', 'delete a file ', ' rm path_to_file'],
+    ['hash', 'calculate a hash for the file ', 'hash path_to_file'],
+    ['compress', 'compress a file ', 'compress path_to_file path_to_destination'],
+    ['decompress', 'decompress a file ', 'decompress path_to_file path_to_destination'],
+    ['exit', 'exit the file manager', 'exit or ctrl+C'],
+  ];
+  console.table(helpList);
+}
+
 export function exitFileManager() {
-  console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+  console.log(`Thank you for using File Manager, ${userName}, goodbye!\n`);
   process.exit();
 }
