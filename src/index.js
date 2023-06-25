00abe96d8f2564809404216645896ee736326bcd
Ekalape <ekalape@outlook.it>
2023-06-25 16:27:50 +0200

    refactor: extract commandslist to a separate file

--- src/index.js
+++ src/index.js
@@ -10,21 +10,21 @@ const homePath = homedir();
 homeDir.set(homePath);
 
 const fullArgs = process.argv.slice(2);
-let uname = 'guest';
 
 if (fullArgs.length === 0 || !fullArgs.find((x) => x.match(/--username=/i)))
   console.log('Please enter your name in format -- --username=your_name');
 else {
   userName.set(fullArgs[0].split('=')[1] || 'guest');
+  const rl = readline.createInterface({ input, output });
   userName.sayHello();
   console.log(`You are currently in ${homePath}`);
   console.log(`Please, enter your command, for full commands list type 'help'`);
 
-  const rl = readline.createInterface({ input, output });
   rl.on('line', (data) => {
     listenCommands(data);
   });
   rl.on('SIGINT', () => {
     exitFileManager();
+    rl.close();
   });
 }
--- src/src/commands.js
+++ src/src/commands.js
@@ -1,7 +1,7 @@
 import { EOL } from 'os';
 import homeDir from './utils/homeDir.js';
 import userName from './utils/userName.js';
-import HelpIndications from './utils/HelpIndications.js';
+import commandsList from './utils/commandsList.js';
 
 const fileOperation = {
   help: commandsList,
@@ -35,38 +35,6 @@ async function listenCommands(data) {
 }
 
 export default listenCommands;
-
-function commandsList() {
-  const helpList = [
-    new HelpIndications('up', 'go upper from current directory', 'up'),
-    new HelpIndications(
-      'cd',
-      'go to dedicated folder from current directory',
-      'cd path_to_directory',
-    ),
-    new HelpIndications('ls', 'print list of all files and folders in current directory', 'ls'),
-    new HelpIndications('add', 'add an empty file to the working directory', 'add new_file_name'),
-    new HelpIndications('cat', 'read a file', 'cat path_to_file'),
-    new HelpIndications('rn', 'rename a file ', 'rn path_to_file new_filename'),
-    new HelpIndications('cp', 'copy a file ', 'cp path_to_file path_to_new_directory'),
-    new HelpIndications('mv', 'move a file ', 'mv path_to_file path_to_new_directory'),
-    new HelpIndications('rm', 'delete a file ', ' rm path_to_file'),
-    new HelpIndications('hash', 'calculate a hash for the file ', 'hash path_to_file'),
-    new HelpIndications(
-      'compress',
-      'compress a file ',
-      'compress path_to_file path_to_destination',
-    ),
-    new HelpIndications(
-      'decompress',
-      'decompress a file ',
-      'decompress path_to_file path_to_destination',
-    ),
-    new HelpIndications('os', 'os commands ', 'os --os_command'),
-    new HelpIndications('.exit', 'exit the file manager', '.exit or ctrl+C'),
-  ];
-  console.table(helpList);
-}
 
 export function exitFileManager() {
   userName.sayBye();
